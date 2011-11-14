219c93388e3839b03b65820c43f775ce2bcc36a0
Esten Hurtle <estenh@estenh.com>
2011-11-14 16:10:16 -0600

    Updated server files.

--- game_server.py
+++ game_server.py
@@ -9,36 +9,86 @@ import re
 import logging
 from pprint import pprint
 sys.path = [os.getcwd()] + sys.path
-from models import User, Melody
+from models import User
 from couchdb import Server, ResourceNotFound
 from collections import defaultdict
 from datetime import datetime, timedelta
 
 class SplashHandler(tornado.web.RequestHandler):
 	@tornado.web.asynchronous
-	def get(self):	
+	def get(self):
+		self.session.invalidate()
 		self.render("templates/splash.html")
 
 class GameHandler(tornado.web.RequestHandler):
         @tornado.web.asynchronous
-        def get(self):  
-		resp = {}
-                self.write(resp)
-		self.finish()
+        def get(self):
+		# Check if user is either null or not a key
+		if self.session.get('user'):
+			user = self.session['user']
+		else:
+			self.redirect("/auth/facebook/")
+			return
+		melodies = user.getMelodies()
+		self.render("templates/game.html",media_url=self.settings["media_url"])
+		return			
 
 class ScoreSaveHandler(tornado.web.RequestHandler):
         @tornado.web.asynchronous
-        def get(self):  
+        def get(self, score):  
                 resp = {}
                 self.write(resp)
                 self.finish()
+	# code for score save to DB for user here
+	def post(self, score):
+		response = {'status': 'failed'}
+		try:
+			user = self.session['user']
+			scores = self.get_argument('scores')
+			if not isinstance(scores, list):
+				self.write(response)
+				self.finish()
+			for item in scores:
+				if not item.isdigit():
+					self.write(response)
+					self.finish()
+		except KeyError:
+			self.write(response)
+			self.finish()
+		user.saveScore(score)	
+		response['status'] = 'succeeded'
+		self.write(response)
+		self.finish()
 
 class MelodySaveHandler(tornado.web.RequestHandler):
         @tornado.web.asynchronous
-        def get(self):  
+        def get(self, melody):  
                 resp = {}
                 self.write(resp)
                 self.finish()
+	# code for melody save to DB for user here
+	def post(self):
+		response = {'status': 'failed'}
+		try: 
+			user = self.session['user']
+			melody = self.get_argument('melody')
+			if not isinstance(melody, list):	
+				self.write(response)
+				self.finish()
+			for item in melody:
+				if not item['freq'].isdigit():
+						self.write(response)
+						self.finish()
+				if not item['dur'].isdigit():
+						self.write(response)
+						self.finish()
+		except KeyError:
+				self.write(response)
+				self.finish()
+		user.saveMelody(melody)
+		response['status'] = 'succeeded'
+		self.write(response)
+		self.finish()
 
 # Facebook auth tends to be a huge pain. I copied this code from iFreeq; most of it should
 # directly translate over.
@@ -60,15 +110,10 @@ class FacebookHandler(tornado.web.RequestHandler, tornado.auth.FacebookGraphMixi
 										extra_params={"scope": "read_stream,publish_stream,offline_access"})
 
 	def _on_login(self, user):
-		active_user = User(id=user["id"],name=user["name"],access_token=user["access_token"])
-		self.session["persistent"]["user"] = active_user
-		if self.session["gamedata"]["scoresave"]:
-			scoresave = True
-			self.session["gamedata"]["scoresave"] = False
-		else:
-			scoresave = False
-			self.session["gamedata"]["scoresave"] = False
-		self.render("templates/auth_close.html")
+		active_user = User(user["id"])
+		self.session["user"] = active_user
+		self.redirect("/initgame")
+		return	
 
 class HighScoreLookup(tornado.web.RequestHandler):
         @tornado.web.asynchronous
@@ -76,3 +121,22 @@ class HighScoreLookup(tornado.web.RequestHandler):
                 resp = {}
                 self.write(resp)
                 self.finish()
+	def post(self):
+		# output user's top 10 high scores
+		response = {'status': 'failed'}
+		try:
+			user = self.session['user']
+			scores = self.get_argument('scores')
+			if not isinstance(scores, list):
+				self.write(response)
+				self.finish()
+			for item in scores:
+				if not item.isdigit():
+					self.write(response)
+					self.finish()
+			highscores = scores.sort()
+			render('highscores.html')
+			return highscores[:9:]
+		except KeyError:
+			self.write(response)
+			self.finish()
--- models.py
+++ models.py
@@ -1,9 +1,58 @@
-class User:
-	#do stuff
-	def save(self):
-		#do save stuff
+from couchdb import Server, ResourceNotFound
+from uuid import uuid4
+import simplejson
 
-class Melody:
-	#do stuff
-	def save(self):
-		#do save stuff
+server = Server()
+
+class User:
+	userid = None
+	initials = None
+	scores = None 
+	melodies=None
+	dbname = 'kp_users'
+
+	def __init__(self, userid):
+		# cause crash if there's a database KeyError. Expected behavior.
+		userdb = server[self.dbname]
+			
+		try:
+			user = userdb[userid]
+			initials = user['initials']
+			scores = user['scores']
+			melodies = user['melodies']
+		except ResourceNotFound:
+			initials = ""
+			scores = []
+			melodies = []
+			userdb[userid] = {'_id':userid, 'initials': initials, 'scores': scores, 'melodies': melodies}
+		
+	def saveScore(self, score):
+		userdb = server[self.dbname]
+		scores.append(score)
+		userdb[self.userid]['scores'] = scores	
+
+	def setInitials(self, itls):
+                userdb = server[self.dbname]
+		initials = itls
+		userdb[self.userid]['initials'] = initials
+
+	def saveMelody(self, melody):
+                userdb = server[self.dbname]
+		m = melody
+		self.melodies.append(m)
+		userdb[self.userid]['melodies']	
+
+	def getUserID(self):
+		return self.userid
+
+	def getMelodies(self):
+                userdb = server[self.dbname]
+		user = userdb[self.userid]
+		return user.get('melodies')
+	
+	def getInitials(self):
+		return self.initials
+
+	def getScores():
+                userdb = server[self.dbname]
+		return userdb[self.userid]['scores']	
